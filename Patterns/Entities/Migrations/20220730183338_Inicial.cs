0bab09dbb37ea6fc98c2e09d1fda35a353b0d66a
Adrian <adrian.nuniez@misca.com.uy>
2022-07-30 16:40:24 -0300

    first commit

---
+++ Patterns/Entities/Migrations/20220730183338_Inicial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artistas",
+                columns: table => new
+                {
+                    ArtistaID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artistas", x => x.ArtistaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Albumes",
+                columns: table => new
+                {
+                    AlbumID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistaID = table.Column<int>(nullable: false),
+                    Titulo = table.Column<string>(nullable: true),
+                    Precio = table.Column<double>(nullable: false),
+                    Anio = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albumes", x => x.AlbumID);
+                    table.ForeignKey(
+                        name: "FK_Albumes_Artistas_ArtistaID",
+                        column: x => x.ArtistaID,
+                        principalTable: "Artistas",
+                        principalColumn: "ArtistaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albumes_ArtistaID",
+                table: "Albumes",
+                column: "ArtistaID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albumes");
+
+            migrationBuilder.DropTable(
+                name: "Artistas");
+        }
+    }
+}
